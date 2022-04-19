082e022505f8b0f5185476d19dd83b73bb716182
easantanders21 <3823@holbertonschool.com>
2022-04-19 15:51:28 -0500

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,11 @@
 # create a simple hostname and ip host entry
-include stdlib
-file_line{'disable password login':
+file_line{ 'disable password login':
   ensure => 'absent',
   path => '/etc/ssh/sshd_config',
   line => '    PasswordAuthentication no',
-  replace => true,
 }
 file_line { 'Identity file':
   ensure => 'absent',
   path => '/etc/ssh/ssh_config',
   line => '    IdentityFile ~/.ssh/school',
-  replace => true,
 }
