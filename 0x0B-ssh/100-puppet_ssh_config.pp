09d7434c5c3ea9961b4f9ddffcbe9d5af9b2a05c
easantanders21 <3823@holbertonschool.com>
2022-04-19 16:24:43 -0500

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Client configuration file (w/ Puppet)
 
-file_line { 'Turn off passwd auth':
+file_line { 'Turn off passwd':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no'
 }
 
-file_line { 'Declare identity file':
+file_line { 'Declare identity':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
