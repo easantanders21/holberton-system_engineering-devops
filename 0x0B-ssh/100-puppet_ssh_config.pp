bb00aff1972d67959615b6db7d883a21bce1c22b
easantanders21 <3823@holbertonschool.com>
2022-04-19 16:14:01 -0500

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,6 +4,7 @@ file_line{ 'Turn off passwd auth':
   path   => '/etc/ssh/sshd_config',
   line   => '    PasswordAuthentication no',
 }
+
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
