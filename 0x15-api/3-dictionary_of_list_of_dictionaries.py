df31c5f35d672344cec4c268eedc5b13bc491663
easantanders21 <3823@holbertonschool.com>
2022-05-19 15:37:52 -0500

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Extend your Python script to export data in the JSON format """
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """ main """
+    file_name = ("todo_all_employees.json")
+    data = {}
+    aux_dict = {}
+
+    # Get information of the users and todos
+    users = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(
+                argv[1])).json()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+                argv[1])).json()
+
+    for user in users:
+        data[user.get('id')] = []
+        aux_dict[user.get('id')] = user.get('username')
+
+    for todo in todos:
+        task_dict = {}
+        task_dict["task"] = todo.get('title')
+        task_dict["completed"] = todo.get("completed")
+        task_dict['username'] = aux_dict.get(todo.get('userId'))
+        data.get(todo.get('userId')).append(task_dict)
+
+    with open(file_name, mode="w") as jsonfile:
+        json.dump(data, jsonfile)
