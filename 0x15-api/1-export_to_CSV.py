6de09a06152287e9f735c742a3026f7e868cd0d5
easantanders21 <3823@holbertonschool.com>
2022-05-19 14:39:13 -0500

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Using what you did in the task #0, extend your Python script to export data in the CSV format."""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    file_name = "{}.csv".format(argv[1])
+    user_id = int(argv[1])
+
+    # Get information of the users and todos
+    users = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(
+                argv[1])).json()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+                            argv[1])).json()
+
+    # create and open a file
+    with open(file_name, mode='w') as csvfile:
+        content = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            content.writerow(
+                [user_id,
+                    users.get('username'),
+                    todo.get('completed'),
+                    todo.get('title')])
