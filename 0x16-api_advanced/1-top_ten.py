c36b48fa0d76adcd32ecc366b0559490003f4f5c
easantanders21 <3823@holbertonschool.com>
2022-05-21 08:11:44 -0500

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,10 +9,10 @@ def top_ten(subreddit):
     header = {'User-Agent': 'my_user_agent'}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     try:
-        response = requests.get(url, headers=headers,
+        response = requests.get(url, headers=header,
                                 allow_redirects=False).json().get("data")
         list_children = response.get("children")
         for childrens in list_children:
             print(childrens.get("data").get("title"))
     except Exception:
-        print("None")
+        print(None)
